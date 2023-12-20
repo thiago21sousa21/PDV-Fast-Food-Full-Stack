0d14ea7397934e17f53fcfb3bf1dba900d472b9a
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-12-19 22:08:03 -0300

    comment: tests

--- back/tests/integration/health.test.ts
+++ back/tests/integration/health.test.ts
@@ -1,19 +1,19 @@
-import supertest from 'supertest'
-import app from '../../src/app'
-import httpStatus from 'http-status';
-import { clearDb } from '../helpers';
+// import supertest from 'supertest'
+// import app from '../../src/app'
+// import httpStatus from 'http-status';
+// import { clearDb } from '../helpers';
 
 
-const api = supertest(app);
+// const api = supertest(app);
 
-describe("health route", ()=>{
+// describe("health route", ()=>{
 
-    beforeEach(async()=>{
-        await clearDb()
-    })
-    it('should return 200', async()=>{
-        const {status, text} = await api.get('/health')
-        expect(status).toBe(httpStatus.OK);
-        expect(text).toBe('I am ok')
-    })
-})
+//     beforeEach(async()=>{
+//         await clearDb()
+//     })
+//     it('should return 200', async()=>{
+//         const {status, text} = await api.get('/health')
+//         expect(status).toBe(httpStatus.OK);
+//         expect(text).toBe('I am ok')
+//     })
+// })
